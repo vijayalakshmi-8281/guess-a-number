6bcf0b145ec507791a94af8e0f99c8b2df1a3e5f
vijayalakshmi-8281 <153008016+vijayalakshmi-8281@users.noreply.github.com>
2023-12-31 16:08:23 +0530

    Add files via upload

---
+++ Guess game.js
@@ -0,0 +1,6 @@
+let gamenum=28;
+let usernum=prompt("enter a number");
+while(usernum!=gamenum){
+    usernum=prompt("sorry you entered a wrong number ! Try again :");
+}
+    console.log("congrats your guess is right");
